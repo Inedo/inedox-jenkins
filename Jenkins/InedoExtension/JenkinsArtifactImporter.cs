0415112db5039985e0e92e7d0e094ee2423a0462
Antony Booth <antony.booth@hodeitech.com>
2019-01-31 14:07:05 -0500

    Modified to allow a subfolder in the Jenkins artifact path to be optionally supplied. This is useful for Javascript framework builds like Angular, that transpile environment configuration into the uglified Javascript, so to promote a build, you build for QA and Production from the same commit and create 2 Buildmaster artifacts

--- Jenkins/InedoExtension/JenkinsArtifactImporter.cs
+++ Jenkins/InedoExtension/JenkinsArtifactImporter.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Inedo.Diagnostics;
-using Inedo.Extensibility;
 using Inedo.Extensibility.Operations;
 using Inedo.IO;
 
@@ -16,6 +15,7 @@ namespace Inedo.Extensions.Jenkins
         public string ArtifactName { get; set; }
         public string JobName { get; set; }
         public string BuildNumber { get; set; }
+        public string SubFolder { get; set; }
 
         public IJenkinsConnectionInfo ConnectionInfo { get; }
         public ILogSink Logger { get; }
@@ -29,21 +29,21 @@ namespace Inedo.Extensions.Jenkins
             if (shim.ApplicationId == null)
                 throw new InvalidOperationException("context requires a valid application ID");
 
-            this.ConnectionInfo = connectionInfo ?? throw new ArgumentNullException(nameof(connectionInfo));
-            this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.Context = shim;
+            ConnectionInfo = connectionInfo ?? throw new ArgumentNullException(nameof(connectionInfo));
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            Context = shim;
         }
 
         public async Task<string> ImportAsync()
         {
-            this.Logger.LogInformation($"Importing artifact \"{this.ArtifactName}\" from Jenkins...");
+            Logger.LogInformation($"Importing artifact \"{ArtifactName}\" from Jenkins...");
 
             string zipFileName = null;
-            string jenkinsBuildNumber = await this.ResolveJenkinsBuildNumber().ConfigureAwait(false);
+            string jenkinsBuildNumber = await ResolveJenkinsBuildNumber().ConfigureAwait(false);
             if (string.IsNullOrEmpty(jenkinsBuildNumber))
             {
-                this.Logger.LogError($"An error occurred attempting to resolve Jenkins build number \"{this.BuildNumber}\". "
-                   + $"This can mean that the special build type was not found, there are no builds for job \"{this.JobName}\", "
+                Logger.LogError($"An error occurred attempting to resolve Jenkins build number \"{BuildNumber}\". "
+                   + $"This can mean that the special build type was not found, there are no builds for job \"{JobName}\", "
                    +"or that the job was not found or is disabled."
                 );
 
@@ -52,25 +52,25 @@ namespace Inedo.Extensions.Jenkins
 
             try
             {
-                this.Logger.LogInformation($"Importing {this.ArtifactName} from {this.JobName}...");
-                var client = new JenkinsClient(this.ConnectionInfo, this.Logger);
+                Logger.LogInformation($"Importing {ArtifactName} from {JobName}...");
+                var client = new JenkinsClient(ConnectionInfo, Logger);
 
                 zipFileName = Path.GetTempFileName();
-                this.Logger.LogDebug("Temp file: " + zipFileName);
+                Logger.LogDebug("Temp file: " + zipFileName);
 
-                this.Logger.LogDebug("Downloading artifact...");
-                await client.DownloadArtifactAsync(this.JobName, jenkinsBuildNumber, zipFileName).ConfigureAwait(false);
-                this.Logger.LogInformation("Artifact downloaded.");
+                Logger.LogDebug("Downloading artifact...");
+                await client.DownloadArtifactAsync(JobName, jenkinsBuildNumber, zipFileName, SubFolder).ConfigureAwait(false);
+                Logger.LogInformation("Artifact downloaded.");
 
                 using (var file = FileEx.Open(zipFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
                 {
                     await SDK.CreateArtifactAsync(
-                        (int)this.Context.ApplicationId,
-                        this.Context.ReleaseNumber,
-                        this.Context.BuildNumber,
-                        this.Context.DeployableId,
-                        this.Context.ExecutionId,
-                        TrimWhitespaceAndZipExtension(this.ArtifactName),
+                        (int)Context.ApplicationId,
+                        Context.ReleaseNumber,
+                        Context.BuildNumber,
+                        Context.DeployableId,
+                        Context.ExecutionId,
+                        TrimWhitespaceAndZipExtension(ArtifactName),
                         file,
                         true
                     ).ConfigureAwait(false);
@@ -82,29 +82,29 @@ namespace Inedo.Extensions.Jenkins
                 {
                     if (zipFileName != null)
                     {
-                        this.Logger.LogDebug("Removing temp file...");
+                        Logger.LogDebug("Removing temp file...");
                         FileEx.Delete(zipFileName);
                     }
                 }
                 catch (Exception ex)
                 {
-                    this.Logger.LogWarning("Error deleting temp file:" + ex.Message);
+                    Logger.LogWarning("Error deleting temp file:" + ex.Message);
                 }
             }
 
-            this.Logger.LogInformation(this.ArtifactName + " artifact imported.");
+            Logger.LogInformation(ArtifactName + " artifact imported.");
 
             return jenkinsBuildNumber;
         }
 
         private Task<string> ResolveJenkinsBuildNumber()
         {
-            if (AH.ParseInt(this.BuildNumber) != null)
-                return Task.FromResult(this.BuildNumber);
+            if (AH.ParseInt(BuildNumber) != null)
+                return Task.FromResult(BuildNumber);
 
-            this.Logger.LogDebug($"Build number is not an integer, resolving special build number \"{this.BuildNumber}\"...");
-            var client = new JenkinsClient(this.ConnectionInfo, this.Logger);
-            return client.GetSpecialBuildNumberAsync(this.JobName, this.BuildNumber);
+            Logger.LogDebug($"Build number is not an integer, resolving special build number \"{BuildNumber}\"...");
+            var client = new JenkinsClient(ConnectionInfo, Logger);
+            return client.GetSpecialBuildNumberAsync(JobName, BuildNumber);
         }
 
         private static string TrimWhitespaceAndZipExtension(string artifactName)
@@ -124,17 +124,17 @@ namespace Inedo.Extensions.Jenkins
             {
                 // this is absolutely horrid, but works for backwards compatibility since this can only be used in BuildMaster
                 this.context = context;
-                this.properties = context.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                properties = context.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
             }
-            public int? ApplicationId => AH.ParseInt(this.GetValue());
-            public int? DeployableId => AH.ParseInt(this.GetValue());
-            public string ReleaseNumber => this.GetValue();
-            public string BuildNumber => this.GetValue();
-            public int ExecutionId => this.context.ExecutionId;
+            public int? ApplicationId => AH.ParseInt(GetValue());
+            public int? DeployableId => AH.ParseInt(GetValue());
+            public string ReleaseNumber => GetValue();
+            public string BuildNumber => GetValue();
+            public int ExecutionId => context.ExecutionId;
             private string GetValue([CallerMemberName] string name = null)
             {
-                var prop = this.properties.FirstOrDefault(p => string.Equals(name, p.Name, StringComparison.OrdinalIgnoreCase));
-                return prop?.GetValue(this.context)?.ToString();
+                var prop = properties.FirstOrDefault(p => string.Equals(name, p.Name, StringComparison.OrdinalIgnoreCase));
+                return prop?.GetValue(context)?.ToString();
             }
         }
     }
--- Jenkins/InedoExtension/Operations/ImportJenkinsArtifactOperation.cs
+++ Jenkins/InedoExtension/Operations/ImportJenkinsArtifactOperation.cs
@@ -46,29 +46,35 @@ namespace Inedo.Extensions.Jenkins.Operations
         [PlaceholderText("e.g. $JenkinsBuildNumber")]
         public string JenkinsBuildNumber { get; set; }
 
+        [ScriptAlias("SubFolder")]
+        [DisplayName("Sub Folder name")]
+        [Description("Optional: The name of the subfolder artifacts to retrieve from Jenkins instead of all the Jenkins Artifact contents i.e: 'output/image' to get image.zip from .../artifact/output/image/*zip*/image.zip instead of .../archive.zip")]
+        public string SubFolder { get; set; }
+
         public async override Task ExecuteAsync(IOperationExecutionContext context)
         {
-            var importer = new JenkinsArtifactImporter((IJenkinsConnectionInfo)this, this, context)
+            var importer = new JenkinsArtifactImporter(this, this, context)
             {
-                ArtifactName = this.ArtifactName,
-                BuildNumber = this.BuildNumber,
-                JobName = this.JobName
+                ArtifactName = ArtifactName,
+                BuildNumber = BuildNumber,
+                JobName = JobName,
+                SubFolder = SubFolder
             };
 
-            this.JenkinsBuildNumber = await importer.ImportAsync().ConfigureAwait(false);
+            JenkinsBuildNumber = await importer.ImportAsync().ConfigureAwait(false);
         }
 
         protected override ExtendedRichDescription GetDescription(IOperationConfiguration config)
         {
-            string buildNumber = config[nameof(this.BuildNumber)];
+            string buildNumber = config[nameof(BuildNumber)];
 
             return new ExtendedRichDescription(
-                new RichDescription("Import Jenkins ", new Hilite(config[nameof(this.ArtifactName)]), " Artifact "),
+                new RichDescription("Import Jenkins ", new Hilite(config[nameof(ArtifactName)]), " Artifact "),
                 new RichDescription("of build ",
                     AH.ParseInt(buildNumber) != null ? "#" : "",
                     new Hilite(buildNumber),
                     " for job ",
-                    new Hilite(config[nameof(this.JobName)])
+                    new Hilite(config[nameof(JobName)])
                 )
             );
         }
