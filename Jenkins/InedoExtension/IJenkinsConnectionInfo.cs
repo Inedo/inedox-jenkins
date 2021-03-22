df15ac27699f76d484d9c1640ceb4faa1d070b9d
Richard Hessinger <rhessinger@inedo.com>
2021-03-22 12:29:32 -0400

    #36 Converted to .net5/net452 and updated the SDK to 1.11

--- Jenkins/InedoExtension/IJenkinsConnectionInfo.cs
+++ Jenkins/InedoExtension/IJenkinsConnectionInfo.cs
@@ -7,7 +7,6 @@ using Inedo.Extensibility.SecureResources;
 using Inedo.Extensions.Credentials;
 using Inedo.Extensions.Jenkins.Credentials;
 using Inedo.Web;
-using Inedo.Web.Plans;
 
 namespace Inedo.Extensions.Jenkins
 {
@@ -61,8 +60,9 @@ namespace Inedo.Extensions.Jenkins
 
         public static (SecureCredentials, JenkinsSecureResource) GetCredentialsAndResource(this IComponentConfiguration config)
         {
-            var editorContext = (IOperationEditorContext)config.EditorContext;
-            return GetCredentialsAndResource(new SuggestionProviderContext(config), new CredentialResolutionContext(editorContext?.ProjectId, null));
+            int? projectId = AH.ParseInt(AH.CoalesceString(config["ProjectId"], config["ApplicationId"]));
+            int? environmentId = AH.ParseInt(config["EnvironmentId"]);
+            return GetCredentialsAndResource(new SuggestionProviderContext(config), new CredentialResolutionContext(projectId, environmentId));
         }
 
         public static (SecureCredentials, JenkinsSecureResource) GetCredentialsAndResource(this IJenkinsConfig operation, ICredentialResolutionContext context)
