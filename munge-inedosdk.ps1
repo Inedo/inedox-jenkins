ba8e04ebf35fd3a199206b6a2b50b1c05b621074
Alex Papadimoulis, Inedo <apapadimoulis@inedo.com>
2022-10-09 01:32:45 +0900

    munge and pack scripts

---
+++ munge-inedosdk.ps1
@@ -0,0 +1,15 @@
+$pkgName = "Inedo.SDK"
+$pkgProjFile= "c:\Projects\Inedo.sdk\Inedo.SDK\Inedo.SDK.csproj"
+
+$slnFile = "C:\Projects\inedox-Jenkins\Jenkins\Jenkins.sln"
+$projFilesToMunge = @( `
+  "C:\Projects\inedox-Jenkins\Jenkins\InedoExtension\InedoExtension.csproj"
+)
+
+dotnet sln "$slnFile" add "$pkgProjFile"
+foreach ($projFile in $projFilesToMunge) {
+  dotnet remove "$projFile" package "$pkgName"
+  dotnet add $projFile reference $pkgProjFile
+}
+
+pause
